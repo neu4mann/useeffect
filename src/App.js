3abb7a4156224be8ac168fdc28f58cdc13bf365b
Paul Neumann <99677188+neu4mann@users.noreply.github.com>
2022-07-17 23:12:34 -0600

    useEffect start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    const [resourceType, setResourceType] = useState('posts')
+
+    useEffect(() => {
+      console.log('render')
+    }, [resourceType])
+
+
+    return (
+    <>
+      <div>
+          <button onClick={() => setResourceType('posts')}></button>
+          <button onClick={() => setResourceType('users')}></button>
+          <button onClick={() => setResourceType('comments')}></button>
+      </div>
+      <h1>{resourceType}</h1>
+    </>     
+  )
 }
-
-export default App;
